164d387bab743df58ee0966c224d336c661c3ea6
kailan457 <kailan457@gmail.com>
2020-04-23 23:48:04 +0300

    initial commit

--- Controller.js
+++ Controller.js
@@ -6,23 +6,25 @@ class Controller
         this.model = model;
         this.clickFunction = null;
         this.clickBlockFunction = null;
+        this.clickCanvasFunction = null;
         this.rightNumbersCount = 0;  
-        this.rightDigits = [];              
+        this.rightDigits = [];  
+        this.showNumberIndices = [false,false,false,false,false]            
     }
     clickButton(event){        
-        this.rightDigits.length = 0;
-        this.rightNumbersCount = 0;
         this.model.resetMistakeCounter();
+        
+        this.view.hideStartButton();        
+        this.view.renderMistakeBlock(model.getCountOfMistakes());
         this.model.setDigits();
-        this.model.shuffle();
-        this.view.resetColors();
-        this.view.hideStartButton();
-        this.view.showDigitsBlock();
-        this.view.render(this.model.getDigits());                
-        this.view.showDigits();
-        this.view.showMistakeBlock();
-        setTimeout(this.view.hideDigits.bind(view),5000);        
-        setTimeout(this.view.setClickBlockFunction.bind(view),5000,this.clickBlockFunction)                    
+        this.view.renderBlocks(model.getDigits());
+        this.showNumberIndices = [false,false,false,false,false]  
+        this.rightDigits = []; 
+        this.rightNumbersCount = 0;
+        this.view.setClickCanvasFunction(undefined); 
+        setTimeout(this.view.hideDigits.bind(view),5000);               
+        setTimeout(this.view.setClickCanvasFunction.bind(view),5000,this.clickCanvasFunction);               
+        this.view.setButtonClickEvent(undefined);
     }
     clickBlock(event){
         var number = view.getNumberOfClickedBlock(event); 
@@ -47,18 +49,50 @@ class Controller
 
                 this.view.showDigit(number);
                 this.view.playAudio("audio/applause.mp3")
+                this.view.setButtonClickEvent(this.clickFunction);        
                 setTimeout(this.view.hideDigitsBlock.bind(view),1000);
                 setTimeout(this.view.showStartButton.bind(view),1000);                                
                 setTimeout(this.view.hideMistakeBlock.bind(view),1000);                                
             }            
         }         
     }
+    clickCanvas(event){
+        var index = this.view.clickCollision(event);        
+        var clickedDigit = this.model.getDigit(index);
+        var rightDigit = this.model.getDigitFromSorted(this.rightNumbersCount)
+        if (this.rightNumbersCount <= 4)
+        {
+            if (rightDigit != clickedDigit && this.rightDigits.indexOf(clickedDigit) == -1){
+                this.model.incrementMistakeCounter();
+                var count = model.getCountOfMistakes();
+                this.view.updateMistakeBlock(count);
+                this.view.playAudio("audio/error.wav");               
+            }
+            else if (this.rightDigits.indexOf(clickedDigit) == -1){                
+                this.showNumberIndices[index] = true;
+                this.view.showDigits(this.model.getDigits(),this.showNumberIndices);                
+                this.rightDigits.push(rightDigit);
+                this.rightNumbersCount++;                
+                this.view.playAudio("audio/success_sound.mp3");
+            }
+            if (this.rightNumbersCount == 5){
+
+                this.showNumberIndices[index] = true;
+                this.view.showDigits(this.model.getDigits(),this.showNumberIndices);
+                this.view.playAudio("audio/applause.mp3")
+                setTimeout(this.view.hideDigitsBlock.bind(view),1000);
+                setTimeout(this.view.renderStartButton.bind(view),1000);                                
+                setTimeout(this.view.hideMistakeBlock.bind(view),1000);       
+                this.view.setButtonClickEvent(this.clickFunction);                             
+            }            
+        }         
+    }
     init(){
         this.clickFunction = this.clickButton.bind(this);
-        view.setButtonClickEvent(this.clickFunction);
-        this.clickBlockFunction = this.clickBlock.bind(this);
-        view.setClickBlockFunction(this.clickBlockFunction);
-        this.view.render(this.model.getDigits());
+        this.view.setButtonClickEvent(this.clickFunction);        
+        this.clickCanvasFunction = this.clickCanvas.bind(this);
+        this.view.setClickCanvasFunction(this.clickCanvasFunction);              
+        this.view.renderStartButton();         
     }
 }
 var controller = new Controller(view,model);
--- View.js
+++ View.js
@@ -1,108 +1,206 @@
 class View
 { 
     constructor(){
-        this.blocksDiv = document.getElementById("blocks");   
-        this.blocksDiv.style.display = "none";            
-        this.button = document.getElementById("button");                
-    }
-    render(digits){        
-        this.blocksDiv.innerHTML = "";
-        for (let i = 0; i < digits.length; i++) {
-            var block = document.createElement("div");
-            block.className="block";
-            var paragraph = document.createElement("p");
-            paragraph.style.display = "none";
-            paragraph.innerText = digits[i];                
-            block.appendChild(paragraph);
-            this.blocksDiv.appendChild(block);          
+        // this.blocksDiv = document.getElementById("blocks");   
+        // if (this.blocksDiv != null)
+        // this.blocksDiv.style.display = "none";                    
+        this.canvas = document.getElementById("canvas");   
+        this.canvas.width = 604;
+        this.canvas.height = 124;         
+        this.context = this.canvas.getContext("2d");        
+        this.context.font = "36pt Verdana";
+        this.canvas.style.display = "block";
+        this.canvasLeft = this.canvas.offsetLeft;
+        this.canvasTop = this.canvas.offsetTop;        
+
+        this.buttonCanvas = document.getElementById("startGameButtonCanvas");
+        this.buttonCanvas.width = 604;
+        this.buttonCanvas.height = 124;
+        this.buttonCanvasContext = this.buttonCanvas.getContext("2d");
+        this.buttonCanvasContext.font = "36pt Verdana";        
+
+        this.mistakesCanvas = document.getElementById("mistakesCount");
+        this.mistakesContext = this.mistakesCanvas.getContext("2d");
+        this.mistakesCanvas.width = 604;
+        this.mistakesCanvas.height = 200;
+        this.mistakesContext.font = "24pt Verdana"                  
+
+        window.onresize = this.updateCanvasPos.bind(this);
+
+        this.blocks = [];
+                        
+        for (let i = 0; i < 5; i++){
+            this.blocks.push({
+                x: i * 120 + 2,
+                y: 2,
+                width: 120,
+                height: 120,                 
+            });
         }
-        this.updateMistakeBlock(0);
-        this.hideMistakeBlock();
-    }        
+
+    }
+
+    renderMistakes(mistakeCount){
+        this.mistakesContext.fillStyle = "white";
+        var text = this.mistakesContext.measureText("Количество ошибок");        
+        this.mistakesContext.fillText("Количество ошибок",this.mistakesCanvas.width / 2 - text.width / 2,80);
+        this.mistakesContext.fillStyle = "yellowgreen";
+        this.mistakesContext.fillRect(this.mistakesCanvas.width / 2 - 60,110,120,90);
+        var mistakeCountText = this.mistakesContext.measureText(mistakeCount);
+        this.mistakesContext.fillStyle = "white";
+        this.mistakesContext.fillText(mistakeCount,this.mistakesCanvas.width / 2 - mistakeCountText.width / 2,165)
+
+    }
+
+    renderBlocks(digits){
+        this.context.fillStyle = "brown";
+        this.context.lineWidth = 2;
+        
+        for (let index = 0; index < this.blocks.length; index++) {
+            const element = this.blocks[index];
+            this.context.fillRect(
+                element.x,
+                element.y,
+                element.width,
+                element.height
+            )
+            this.context.strokeRect(
+                element.x,
+                element.y,
+                element.width,
+                element.height
+            )                    
+        }
+        this.context.fillStyle = "white"; 
+        if (digits != null)
+        for(let i = 0; i < 5; i++) {
+            var text =this.context.measureText(digits[i]);
+            this.context.fillText(digits[i],i * 120 + 60 - text.width / 2,80);
+        }   
+    }
+
+    renderStartButton(){
+        this.buttonCanvasContext.fillStyle = "yellowgreen";
+        this.buttonCanvasContext.fillRect(0,0,604,124);
+        this.buttonCanvasContext.fillStyle = "white";    
+        var text = this.buttonCanvasContext.measureText("Начать игру");
+        this.buttonCanvasContext.fillText("Начать игру",604 / 2 - text.width / 2,80);
+    }
+
+    render(digits){            
+        // this.renderMistakes(0);
+        // this.renderBlocks(digits);
+        this.renderStartButton();
+    }    
+
+    updateCanvasPos(){
+        var canvas = document.getElementById("canvas");
+        this.canvasLeft = canvas.offsetLeft;
+        this.canvasTop = canvas.offsetTop;        
+    }
+
     setButtonClickEvent(clickFunction){
-        this.button.onclick = clickFunction;                
-    }
-    alertNumber(number){
-        alert(number);
-    }
-    getNumberOfClickedBlock(event){
-        var blocks = document.getElementsByClassName("block");
-        for (let index = 0; index < blocks.length; index++) {
-            const element = blocks[index];
-            if (element == event.currentTarget){
-                return index;
-            }
-        }
-    }
-    setClickBlockFunction(clickBlockFunction){
-        var blocks = document.getElementsByClassName("block");
-        for (let index = 0; index < blocks.length; index++) {
-            const element = blocks[index];
-            element.onclick = clickBlockFunction;
-        }
-    }
-    showDigits(){
-        var blocks = document.getElementsByClassName("block");
-        for (let i = 0; i < blocks.length; i++) {
-            const child = blocks[i].firstChild;            
-            child.style.display = "block";
-        }
-    }
+        if (this.buttonCanvas != null)
+        this.buttonCanvas.onclick = clickFunction;               
+    }    
+    
     hideDigits(){
-        var blocks = document.getElementsByClassName("block");
-        for (let i = 0; i < blocks.length; i++) {
-            const child = blocks[i].firstChild;            
-            child.style.display = "none";
-        }
+        this.hideDigitsBlock();
+        this.renderBlocks(null);        
     }
-    showDigit(blockIndex){
-        var blocks = document.getElementsByClassName("block");
-        for (let i = 0; i < blocks.length; i++) {            
-            if (i == blockIndex){
-                blocks[i].style.backgroundColor = "green";
-                const child = blocks[i].firstChild;
-                child.style.display = "block";
+
+    showDigits(digits,showNumberIndices){
+        this.hideDigitsBlock();
+        this.context.fillStyle = "brown";
+        this.context.lineWidth = 2;
+        var flag = false;
+        for (let index = 0; index < this.blocks.length; index++) {
+            if (showNumberIndices[index] == true){
+                this.context.fillStyle = "green";
             }
+            else{
+                this.context.fillStyle = "brown";
+            }
+            const element = this.blocks[index];
+            this.context.fillRect(
+                element.x,
+                element.y,
+                element.width,
+                element.height
+            )
+            this.context.strokeRect(
+                element.x,
+                element.y,
+                element.width,
+                element.height
+            )                    
         }
+        this.context.fillStyle = "white"; 
+        if (digits != null)
+        for(let i = 0; i < 5; i++) {
+            if (showNumberIndices[i] == true){
+                var text =this.context.measureText(digits[i]);
+                this.context.fillText(digits[i],i * 120 + 60 - text.width / 2,80);    
+            }            
+        }   
     }
+
     resetColors(){
         var blocks = document.getElementsByClassName("block");
+        if (blocks != null)
         for (let i = 0; i < blocks.length; i++) {                        
                 blocks[i].style.backgroundColor = "dimgray";                            
         }
     }
+
     hideStartButton(){
-        this.button.style.display = "none";
-    }
-    showStartButton(){
-        this.button.style.display = "block";
-    }
+        this.buttonCanvasContext.clearRect(0,0,this.buttonCanvas.width,this.buttonCanvas.height);
+    }    
+
     hideDigitsBlock(){
-        this.blocksDiv.style.display = "none";        
+        this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
     }
+
     showDigitsBlock(){
+        if (this.button != null)
         this.blocksDiv.style.display = "block";
     }
+
     updateMistakeBlock(count){
-        var mistakeCounter = document.getElementById("mistakeCounter");
-        var p = mistakeCounter.firstElementChild;
-        p.innerText = count;
+        this.hideMistakeBlock();
+        this.renderMistakeBlock(count);
     }
+
     hideMistakeBlock(){
-        var mistakeCounter = document.getElementById("mistakeCounter");
-        var mistakeCounterLabel = document.getElementById("mistakeCounterLabel");        
-        mistakeCounter.style.display = "none";
-        mistakeCounterLabel.style.display = "none";
+        this.mistakesContext.clearRect(0,0,this.mistakesCanvas.width,this.mistakesCanvas.height);
     }
-    showMistakeBlock(){
-        var mistakeCounter = document.getElementById("mistakeCounter");
-        var mistakeCounterLabel = document.getElementById("mistakeCounterLabel");    
-        mistakeCounter.style.display = "block";
-        mistakeCounterLabel.style.display = "block";
+
+    renderMistakeBlock(mistakesCount){
+        this.renderMistakes(mistakesCount);
     }
+
     playAudio(path){
         var audio = new Audio(path);
         audio.play();
     }
+
+    setClickCanvasFunction(clickCanvasFunction){
+        if (canvas != null)
+        this.canvas.onclick = clickCanvasFunction;
+    }
+
+    clickCollision(event){
+        var x = event.offsetX;
+        var y = event.offsetY;
+
+        for (let i = 0; i < this.blocks.length; i++) {
+            const element = this.blocks[i];
+            if (y > element.y && y < element.y + element.height 
+                && x > element.x && x < element.x + element.width) {
+                return i;
+            }
+        }        
+    }
 }
+
 var view = new View();
