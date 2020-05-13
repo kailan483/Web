77ed330c6256ab9c66450eddf52e5483f2df0d20
kailan457 <kailan457@gmail.com>
2020-05-13 15:18:04 +0300

    small modifications

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Footer from './Footer';
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<Header/>, document.body);
-ReactDOM.render(<body><App/><Footer/></body>, document.body);
+ReactDOM.render(<body><App/></body>, document.body);
 // ReactDOM.render(<Footer/>, document.body);
 
 // If you want your app to work offline and load faster, you can change
